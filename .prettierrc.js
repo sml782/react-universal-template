181779960f11e1ec55db186f003c9f4a505e834c
SGoldL <782015430@qq.com>
2019-04-19 23:52:45 +0800

    feat: 添加prettierrc

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  // useTabs: false,
+  semi: true,
+  singleQuete: true,
+  trailingComma: 'all',
+  // bracketSpacing: true,
+  // jsxBracketSameLine: false,
+  // arrowParens: 'avoid',
+  // rangeStart: 0,
+  // rangeEnd: Infinity,
+  // poseWrap: 'preserve'
+};
